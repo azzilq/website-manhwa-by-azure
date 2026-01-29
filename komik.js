8862b122400561f078131aa3f2f4ec1150c80812
azure <llynn8024@gmail.com>
2026-01-29 21:04:46 +0700

    Update komik.js

--- komik.js
+++ komik.js
@@ -1,9 +1,14 @@
 const komik = [
 {
-id:1,
-judul:"How to Send Your Husband to Hell",
-cover:"https://drive.google.com/uc?export=view&id=1wWi_W6-cEA4TZA2J9w0jd50ZKBI3mEpy",
-genre:["Romance","Josei"],
-status:"Ongoing"
+{
+  id: "How to Send Your Husband to Hell",
+  judul: "How to Send Your Husband to Hell",
+  cover: "https://via.placeholder.com/300x450",
+  genre: ["Romance,Historical"],
+  status: "Ongoing",
+  chapterTerbaru: 1,
+  update: "2026-01-29",
+  deskripsi: "Helena Winston, putri count yang mempesona, terseret ke dalam skandal ketika putra mahkota tiba-tiba menyatakan cinta padanya. Demi melindungi kekaisaran dan reputasinya sendiri, ia mengiakan kesepakatan rahasia dengan permaisuri. Helena harus menikahi Count Benjamin Ischpern, seorang bangsawan misterius yang rumornya memiliki hubungan dengan musuh kekaisaran, dan menguak semua rahasianya!."
+},
 }
 ];
