b0fd166cd08377857dfed2aa824b21049e8c7d1c
azure <llynn8024@gmail.com>
2026-01-29 20:32:28 +0700

    Create komik.js

---
+++ komik.js
@@ -0,0 +1,9 @@
+const komik = [
+{
+id:1,
+judul:"How to Send Your Husband to Hell",
+cover:"https://drive.google.com/uc?export=view&id=1wWi_W6-cEA4TZA2J9w0jd50ZKBI3mEpy",
+genre:["Romance","Josei"],
+status:"Ongoing"
+}
+];
